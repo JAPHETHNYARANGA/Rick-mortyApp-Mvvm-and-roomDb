db9f7dface1e661b1ba5a0ffa01e48fa500282ec
JAPHETHNYARANGA <nyaranga4@gmail.com>
2022-04-15 17:52:22 +0300

    use the project upto this commit to check if there is response from your Api

--- app/src/main/java/com/storesoko/rickmortyapp/MainActivity.kt
+++ app/src/main/java/com/storesoko/rickmortyapp/MainActivity.kt
@@ -2,10 +2,33 @@ package com.storesoko.rickmortyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.storesoko.rickmortyapp.Network.Retrofit.ApiClient
+import com.storesoko.rickmortyapp.Network.models.CharacterResponse
+import retrofit2.Call
+import retrofit2.Response
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val client = ApiClient.apiService.fetchCharacters("1")
+        client.enqueue(object : retrofit2.Callback<CharacterResponse>{
+            override fun onResponse(
+                call: Call<CharacterResponse>,
+                response: Response<CharacterResponse>
+            ) {
+                if(response.isSuccessful){
+                    Log.i("response1", "Successful response"+response.body())
+                }
+            }
+
+            override fun onFailure(call: Call<CharacterResponse>, t: Throwable) {
+                Log.e("failed",""+t.message)
+            }
+        })
     }
 }
